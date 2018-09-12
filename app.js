9da2ef328e4dc89f07e1216391b539ba196cc002
IshmeenChawla <ishychawla@gmail.com>
2018-09-12 19:03:05 +1000

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ var logger = require('morgan');
 //New Code
 var mongo = require('mongodb');
 var monk = require('monk');
-var db = monk('localhost:27017/nodetest1');
+var db = process.env.MONGODB_URI || monk('localhost:27017/nodetest1');
+//var db = monk('localhost:27017/nodetest1');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
